5fbbd4dadbb183d4a2608730cebc3692bfa632a0
Harman Grewal <Harman Grewal>
2017-02-17 20:20:50 +0530

    Chack

--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -1,17 +1,18 @@
 import {Component} from "@angular/core";
 @Component({
     selector: 'app-header',
-    template:' <header class="row"> ' +
-    '<nav class="col-md-8 col-md-offset-2">' +
-    ' <ul class="nav nav-pills">' +
-    '<li>' +
-    '<a>Messanger</a>' +
-    '</li>' +
-    '<li>' +
-    '<a>Authentication</a>' +
-    '</li> ' +
-    '</ul> ' +
-    '</nav> </header>'
+
+    template: `
+
+<header class="row">
+<nav class="col-md-8 col-md-offset-2">
+<ul class="nav nav-pills">
+<li><a [routerLink] = "['/messages']">Messanger</a></li>
+<li><a [routerLink] = "['/auth']" >Authentication</a></li>
+</ul>
+</nav>
+</header>
+    `
 
 })
 
